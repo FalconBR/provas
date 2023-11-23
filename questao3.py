a85afe914c75a020210ce93b3469e666d8aa40fa
Rafael Monteiro Rodrigues <42948262+mrafaelr@users.noreply.github.com>
2023-11-23 14:52:19 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -1,1 +1,22 @@
+def decodificar_huffman(codigo, tabela):
+    mensagem_decodificada = ""
+    codigo_atual = ""
 
+    for bit in codigo:
+        codigo_atual += bit
+
+        # Verifica se o código atual está na tabela
+        for caractere, cod in tabela.items():
+            if cod == codigo_atual:
+                mensagem_decodificada += caractere
+                codigo_atual = ""  # Reinicia o código atual
+
+    return mensagem_decodificada
+
+# Tabela de codificação Huffman corrigida
+tabela_huffman_corrigida = {'A': '0', 'B': '111', 'R': '1100', 'C': '1101', 'D': '10'}
+
+# Exemplo de uso com a mensagem "BARCA"
+codigo_corrigido = '11101011000'
+mensagem_original_corrigida = decodificar_huffman(codigo_corrigido, tabela_huffman_corrigida)
+print("Mensagem decodificada:", mensagem_original_corrigida)
