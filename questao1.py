ae0da54a70b643221f65d7cb5faa71d036620ecd
Rafael Monteiro Rodrigues <42948262+mrafaelr@users.noreply.github.com>
2023-11-23 14:19:08 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,1 +1,16 @@
-print ("Hello world")
+def calcular_quadrados_concatenar(numero):
+    # Converte o número para uma string para iterar sobre seus dígitos
+    numero_str = str(numero)
+
+    # Calcula os quadrados dos dígitos e os concatena
+    resultado = ''.join(str(int(digito) ** 2) for digito in numero_str)
+
+    # Converte o resultado de volta para um número inteiro
+    resultado_int = int(resultado)
+
+    return resultado_int
+
+# Exemplo de uso:
+numero = 6122
+resultado = calcular_quadrados_concatenar(numero)
+print(resultado)
