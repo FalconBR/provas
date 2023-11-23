b9e3e6486b7002daa3019bab728da11f83e2709c
FalconBR <tbrunacci@gmail.com>
2023-11-23 15:17:30 -0300

    Update todas.py

--- todas.py
+++ todas.py
@@ -38,3 +38,32 @@ print("Algarismos que nunca aparecem na casa das unidades:", algarismos_nao_apar
 # Fim questao 1 ----------------------
 
 # Questao 2 
+
+# Item a
+
+def indice_primeiro_termo_fibonacci_com_n_digitos(n):
+    fib = [0, 1]  # Inicializa os dois primeiros termos da sequência de Fibonacci
+    index = 2  # Índice inicial
+
+    while True:
+        next_term = fib[-1] + fib[-2]
+        fib.append(next_term)
+
+        # Verifica se o próximo termo tem n dígitos
+        if len(str(next_term)) == n:
+            return index
+
+        index += 1
+
+# Exemplo de uso:
+n = 5
+resultado = indice_primeiro_termo_fibonacci_com_n_digitos(n)
+print("O índice do primeiro termo da sequência de Fibonacci com", n, "dígitos é:", resultado)
+
+# Item b
+
+O primeiro elemento da sequencia com 1000 dígitos ocorre na posição 4782
+
+# Fim questao 2 ----------------------
+
+# Questao 3
